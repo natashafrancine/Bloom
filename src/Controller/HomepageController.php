0cd684a884249e1aa11974c56ea7dcdc72ed4021
natashafrancine <natashafrancinenazarino@gmail.com>
2025-12-12 07:07:27 +0800

    log in

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,17 +2,44 @@
 
 namespace App\Controller;
 
+use App\Repository\ActivityLogRepository;
+use App\Repository\CategoryRepository;
+use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
+    public function __construct(
+        private ProductRepository $productRepository,
+        private OrderRepository $orderRepository,
+        private UserRepository $userRepository,
+        private CategoryRepository $categoryRepository,
+        private ActivityLogRepository $activityLogRepository
+    ) {}
+
     #[Route('/', name: 'app_root')]
     #[Route('/home', name: 'app_home')]
     public function home(): Response
     {
-        return $this->redirectToRoute('product_index');
+        $stats = [
+            'totalProducts' => $this->productRepository->count([]),
+            'totalOrders' => $this->orderRepository->count([]),
+            'totalUsers' => $this->userRepository->count([]),
+            'totalCategories' => $this->categoryRepository->count([]),
+        ];
+
+        $recentOrders = $this->orderRepository->findRecent(5);
+        $recentActivities = $this->activityLogRepository->findBy([], ['createdAt' => 'DESC'], 5);
+
+        return $this->render('homepage/dashboard.html.twig', [
+            'stats' => $stats,
+            'recentOrders' => $recentOrders,
+            'recentActivities' => $recentActivities,
+        ]);
     }
 
     #[Route('/about', name: 'app_about')]
