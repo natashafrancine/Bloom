c68bf92cd3da417fa3fd2c8b48bc01e7c5683597
natashafrancine <natashafrancinenazarino@gmail.com>
2025-12-12 10:07:40 +0800

    user one role display

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -72,8 +72,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
-
+        
         return array_unique($roles);
     }
 
